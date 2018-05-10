eb4fc15c9299ea616637d179320c9149f9f6278a
daryll <dosis@csumb.edu>
2018-05-10 16:30:49 -0700

    fixed the server

--- server.js
+++ server.js
@@ -55,56 +55,88 @@ app.get('/success', function(req, res) {
 
 app.post('/register', function(req, res) {
     var password = req.body.password;
+    var isValid = true;
     bcrypt.hash(password, 10, function(err, hash) {
         if(err) {
             console.log(err);
             res.redirect('/');
         } else {
-            var isValid = true;
             //query to see if the email exists
-            var emaildup = User.find({email: req.body.email});
-
-            //if email is already in the database
-            if(emaildup) {
-                console.log("email is already taken");
-                isValid = false;
-            }
-            if(req.body.email.length < 6) {
-                console.log("email should be longer than 6");
-                isValid = false;
-            } 
-            if(req.body.first_name < 1) {
-                console.log("first name can't be empty");
-                isValid = false;
-            } 
-            if(req.body.last_name < 1) {
-                console.log("last name can't be empty");
-                isValid = false;
-            } 
-            if(req.body.password < 6) {
-                console.log("password should be longer than 6 characters");
-                isValid = false;
-            } 
-            
-            if(isValid == true) {
-                var register = new User({email: req.body.email, first_name: req.body.first_name, last_name: req.body.last_name, password: hash});
-                register.save();
-                console.log("successfully registered");
-                req.session.first_name = register.first_name;
-                req.session.userid = register._id;
-                //check if the user is logged in
-                console.log("user id is " + req.session.userid);
-
-                req.session.isloggedin = true;
-                res.redirect('/success');
-                
-            } else {
-                res.redirect('/')  
-            }
-        //redirects to root route if the the user fails the validations. 
+            User.findOne({email: req.body.email}, function(err, emaildup) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    if(emaildup) {
+                        console.log("email is already taken");
+                        isValid = false;
+                    } 
+                    if(req.body.email.length < 6) {
+                        console.log("email should be longer than 6");
+                        isValid = false;
+                    } 
+                    if(req.body.first_name < 1) {
+                        console.log("first name can't be empty");
+                        isValid = false;
+                    } 
+                    if(req.body.last_name < 1) {
+                        console.log("last name can't be empty");
+                        isValid = false;
+                    } 
+                    if(req.body.password < 6) {
+                        console.log("password should be longer than 6 characters");
+                        isValid = false;
+                    } 
+                    
+                    if(isValid == true) {
+                        var register = new User({email: req.body.email, first_name: req.body.first_name, last_name: req.body.last_name, password: hash});
+                        register.save();
+                        console.log("successfully registered");
+                        req.session.first_name = register.first_name;
+                        req.session.userid = register._id;
+                        //check if the user is logged in
+                        console.log("user id is " + req.session.userid);
+        
+                        req.session.isloggedin = true;
+                        res.redirect('/success');
+                    } else {
+                        res.redirect('/')  
+                    }
+                }
+            });
         }
-      })
+    })
 });
+
+app.post('/login', function(req, res) {
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err) {
+            console.log("can't login");
+            res.redirect('/')
+        } else {
+            if(user){
+                const typedpass = req.body.password;
+                const hashedpass = user.password;
+                bcrypt.compare(typedpass, hashedpass, function(err, correctpass) {
+                    if(correctpass) {
+                        console.log("you did it! logged in!");
+                        req.session.first_name = user.first_name;
+                        req.session.userid = user._id;
+                        //check if the user is logged in
+                        req.session.isloggedin = true;
+                        res.redirect('/success');
+                    } else {
+                        console.log("incorrect password");
+                        res.redirect('/');
+                    } 
+                });
+            } else {
+                console.log("user doesnt exist");
+                res.redirect('/')
+            } 
+        } 
+    })
+})
+
 
 app.post('/login', function(req, res) {
     User.findOne({email: req.body.email}, function(err, user){
